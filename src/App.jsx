ce3c4ab39c8e90a4181f27f3e5669cc55a110232
H82M19\Student <Student>
2025-02-19 17:36:35 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import B1 from './conponents/b1'
+import B2 from './conponents/b2'
+import B3 from './conponents/b3'
+import BUTTON from './conponents/button'
+function App() {
+  
+  return (
+    <>
+      {/* <B1></B1>
+      <B2></B2>
+      <B3></B3> */}
+      <BUTTON></BUTTON>
+    </>
+  )
+}
+
+export default App
